74791c6fb935e4cf1ba7b4acd2fdd297f55a00cc
choleece <choleece@163.com>
2020-10-17 00:01:40 +0800

    dp

--- src/main/java/cn/choleece/base/leetcode/Solution53V2.java
+++ src/main/java/cn/choleece/base/leetcode/Solution53V2.java
@@ -1,5 +1,7 @@
 package cn.choleece.base.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author choleece
  * @Description: TODO
@@ -35,6 +37,50 @@ public class Solution53V2 {
         return maxSum;
     }
 
+    /**
+     * 动态规划的做法
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 状态为以i结尾的连续的子数组的最大值
+            // 状态转移方程
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    /**
+     * 动态规划的做法
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int max = Integer.MIN_VALUE;
+        // 用一个字段接收
+        int dp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 状态为以i结尾的连续的子数组的最大值
+            // 状态转移方程
+            dp = Math.max(dp + nums[i], nums[i]);
+            max = Math.max(max, dp);
+        }
+
+        return max;
+    }
+
     public static int maxSubArray1(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -57,5 +103,9 @@ public class Solution53V2 {
         System.out.println("max sum: " + maxSubArray(nums));
 
         System.out.println("max sum1: " + maxSubArray1(nums));
+
+        System.out.println("max sum2: " + maxSubArray2(nums));
+
+        System.out.println("max sum3: " + maxSubArray3(nums));
     }
 }
