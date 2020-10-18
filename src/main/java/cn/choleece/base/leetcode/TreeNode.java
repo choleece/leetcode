05fc56246765e7788d1f468ae1a578d9d04a4dc6
choleece <choleece@163.com>
2020-10-18 16:52:33 +0800

    dfs

--- src/main/java/cn/choleece/base/leetcode/TreeNode.java
+++ src/main/java/cn/choleece/base/leetcode/TreeNode.java
@@ -1,5 +1,7 @@
 package cn.choleece.base.leetcode;
 
+import java.util.Stack;
+
 /**
  * @author choleece
  */
@@ -25,5 +27,28 @@ public class TreeNode {
         TreeNode root = new TreeNode(0);
 
         return root;
+    }
+
+    public static void dfs(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode tmp = stack.pop();
+            System.out.println(tmp.val);
+
+            if (tmp.right != null) {
+                stack.push(tmp.right);
+            }
+
+            if (tmp.left != null) {
+                stack.push(tmp.left);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(5, new TreeNode(6), null)), new TreeNode(4));
+        dfs(root);
     }
 }
